a4b01cb345e24db5f62933ce728d935fb09f67ce
raafaelnunes12 <72103627+raafaelnunes12@users.noreply.github.com>
2020-09-30 12:06:22 -0300

    Correção bug valor menor que 10

--- Javascript/js/form.js
+++ Javascript/js/form.js
@@ -1,43 +1,44 @@
 var botaoAdicionar = document.querySelector("#adicionar-jogo"); // Localiza o botão Adicionar no form Html
-botaoAdicionar.addEventListener("click", function(event) { // Associa o evento click do botão
-    event.preventDefault();    // chama função para restaurar o comportamento padrão do botão
+botaoAdicionar.addEventListener("click", function(event) {      // Associa o evento click do botão
+    event.preventDefault();                                     // chama função para restaurar o comportamento padrão do botão
 
     // adicionando o registro do jogo na tabela
     var form = document.querySelector("#form-adiciona");  //seleciona os dados informados no formulário
-    var registro = obtemJogoDoFormulario(form); // Armazena os dados retornados do Jogo
+    var registro = obtemJogoDoFormulario(form);           // Armazena os dados retornados do Jogo
 
-    var erroJogo = validaJogo(registro.codJogo); //Armazena o retorno da função validaJogo
-    var erroPlacar = validaPlacar(registro.placar); //Armazena o retorno da função validaPlacar
+    var erroJogo = validaJogo(registro.codJogo);     //Armazena o retorno da função validaJogo
+    var erroPlacar = validaPlacar(registro.placar);  //Armazena o retorno da função validaPlacar
     
-    if (erroJogo.length > 0){    // Verifica se houve alguma mensagem de erro, comparando se o retorna da variável erroJogo, que é uma string, é maior do que 0.
+    if (erroJogo.length > 0){                                     // Verifica se houve alguma mensagem de erro, comparando se o retorna da variável erroJogo, que é uma string, é maior do que 0.
         var mensagemErro = document.querySelector("#erro-jogo");  // Grava o retorno da função validaJogo na tag Span com o id erro-jogo
-        mensagemErro.textContent = erroJogo;   // Grava o retorno da função validaJogo na tag Span com o id erro-jogo
+        mensagemErro.textContent = erroJogo;                      // Grava o retorno da função validaJogo na tag Span com o id erro-jogo
         return;
     }
 
-    if (erroPlacar.length > 0) {       // Verifica se houve alguma mensagem de erro, comparando se o retorna da variável erroPlacar, que é uma string, é maior do que 0.
+    if (erroPlacar.length > 0) {                                   // Verifica se houve alguma mensagem de erro, comparando se o retorna da variável erroPlacar, que é uma string, é maior do que 0.
         var mensagemErro = document.querySelector("#erro-placar"); // Grava o retorno da função validaPlacar na tag Span com o id erro-placar
-        mensagemErro.textContent = erroPlacar;  // Grava o retorno da função validaPlacar na tag Span com o id erro-placar
+        mensagemErro.textContent = erroPlacar;                     // Grava o retorno da função validaPlacar na tag Span com o id erro-placar
         return;
     }
+    
     adicionaJogoNaTabela(registro);  //Monta a estrutura da table do id tabela-jogos    
  
     form.reset();  // Limpa todas as informações dos campos do form
 
     var errosJogo = document.querySelector("#erro-jogo");
     var errosPlacar = document.querySelector("#erro-placar");
-    errosJogo.innerHTML = "";
+    errosJogo.innerHTML = "";                                      //Remove as mensagens de validação apresentadas
     errosPlacar.innerHTML = "";
 });
 
-function obtemJogoDoFormulario(form) {   //Recebe as informações da variável jogo;
-    var jogo = {                         //Inicia variável
-        codJogo: form.jogo.value,           //Informação do campo Jogo do form
-        placar: form.placar.value,       //Informação do campo Placar do form        
-        minimoTemporada: calculaMinimoTemporada(form.placar.value), //Retorno da função calculaMinimoTemporada
+function obtemJogoDoFormulario(form) {                                      //Recebe as informações da variável jogo;
+    var jogo = {                                                            //Inicia variável
+        codJogo: form.jogo.value,                                           //Informação do campo Jogo do form
+        placar: form.placar.value,                                          //Informação do campo Placar do form        
+        minimoTemporada: calculaMinimoTemporada(Number(form.placar.value)), //Retorno da função calculaMinimoTemporada
         maximoTemporada: calculaMaximoTemporada(Number(form.placar.value)), //Retorno da função calculaMaximoTemporada  
-        recordeMinino: quebraMinimo, //Retorno da variável quebraMinimo
-        recordeMaximo: quebraMaximo  //Retorno da variável quebraMaximo       
+        recordeMinino: quebraMinimo,                                        //Retorno da variável quebraMinimo
+        recordeMaximo: quebraMaximo                                         //Retorno da variável quebraMaximo       
     }
     return jogo;
 }
